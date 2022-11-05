6b22d9770cf72978e885315522640134a1f5b6ba
Nattanun Sornsurin <potaaee@gmail.com>
2022-11-05 17:59:32 +0700

    edit menu

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:tms/state_management.dart';
 import 'package:tms/pages/home.dart';
 import 'package:tms/pages/sales/sales.dart';
-import 'package:tms/pages/news.dart';
+import 'package:tms/pages/news/news.dart';
+import 'package:tms/theme/color.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -28,10 +30,16 @@ class _MenuState extends State<Menu> {
                 selectedFontSize: 16,
                 unselectedFontSize: 14,
                 backgroundColor: Colors.white,
-                items: const [
-                  BottomNavigationBarItem(icon: Icon(Icons.home), label: 'หน้าหลัก'),
-                  BottomNavigationBarItem(icon: Icon(Icons.signal_cellular_alt), label: 'ยอดขาย'),
-                  BottomNavigationBarItem(icon: Icon(Icons.campaign), label: 'ข่าวสาร'),
+                items: [
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset('assets/icons/House.svg', color: (Store.currentIndex.value == 0) ? ThemeColor.primaryColor : null),
+                      label: 'หน้าหลัก'),
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset('assets/icons/Graph up.svg', color: (Store.currentIndex.value == 1) ? ThemeColor.primaryColor : null),
+                      label: 'ยอดขาย'),
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset('assets/icons/megaphone.svg', color: (Store.currentIndex.value == 2) ? ThemeColor.primaryColor : null),
+                      label: 'ข่าวสาร'),
                 ],
                 onTap: (index) => Store.currentIndex.value = index,
               )
