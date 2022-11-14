92b0a77319fe30244a20a08f28fe1b2603d972ac
Nattanun Sornsurin <potaaee@gmail.com>
2022-11-14 11:11:01 +0700

    edit color icon

--- lib/widgets/list_sales.dart
+++ lib/widgets/list_sales.dart
@@ -14,43 +14,46 @@ Widget listSales({
 }) {
   return Container(
     decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.grey)),
-    child: ExpandablePanel(
-      theme: const ExpandableThemeData(
-        headerAlignment: ExpandablePanelHeaderAlignment.center,
-        useInkWell: false,
-      ),
-      header: ListTile(
-        leading: SvgPicture.asset(icon),
-        title: text(title, fontSize: 24),
-        trailing: Wrap(children: [
-          text(quantity, fontSize: 24, color: ThemeColor.primaryColor),
-          const SizedBox(width: 5),
-          text(unit, fontSize: 24),
-        ]),
-      ),
-      expanded: Column(
-        children: [
-          Container(
-            color: ThemeColor.primaryColor,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10, right: 20),
-              child: ListTile(
-                leading: text('เครื่องและรุ่น', color: Colors.white),
-                trailing: text('ยอดขาย', color: Colors.white),
+    child: ExpandableTheme(
+      data: const ExpandableThemeData(iconColor: ThemeColor.primaryColor),
+      child: ExpandablePanel(
+        theme: const ExpandableThemeData(
+          headerAlignment: ExpandablePanelHeaderAlignment.center,
+          useInkWell: false,
+        ),
+        header: ListTile(
+          leading: SvgPicture.asset(icon),
+          title: text(title, fontSize: 24),
+          trailing: Wrap(children: [
+            text(quantity, fontSize: 24, color: ThemeColor.primaryColor),
+            const SizedBox(width: 5),
+            text(unit, fontSize: 24),
+          ]),
+        ),
+        expanded: Column(
+          children: [
+            Container(
+              color: ThemeColor.primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 20),
+                child: ListTile(
+                  leading: text('เครื่องและรุ่น', color: Colors.white),
+                  trailing: text('ยอดขาย', color: Colors.white),
+                ),
               ),
             ),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            padding: EdgeInsets.zero,
-            itemCount: 2,
-            itemBuilder: (context, index) {
-              return listSalesDetail(title: 'ซิมฟันธง', quantity: '2', unit: 'ซิม');
-            },
-          ),
-        ],
+            ListView.builder(
+              shrinkWrap: true,
+              padding: EdgeInsets.zero,
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return listSalesDetail(title: 'ซิมฟันธง', quantity: '2', unit: 'ซิม');
+              },
+            ),
+          ],
+        ),
+        collapsed: const SizedBox(),
       ),
-      collapsed: const SizedBox(),
     ),
   ).paddingSymmetric(horizontal: 15, vertical: 10);
 }
